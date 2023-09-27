cc1cbb7a51265ff4dda2840981877b9a61f0f610
Leiciane <leicianesouza@controladoria.mt.gov.br>
2023-09-27 14:11:53 -0400

    ip api web

---
+++ api/src/scripts/populate.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+const { UsuarioModel } = require("../models/usuario-model");
+const usuarios = [
+ {
+  nome: "Admin",
+  matricula: 123456,
+  email: "admin@email.com",
+  telefone: "(65)99988-7766",
+  perfil: "admin",
+  cpf: "123.456.789-10",
+ },
+];
+async function criarUsuarios() {
+ for (let usuario of usuarios) {
+  await UsuarioModel.create({
+   nome: usuario.nome,
+   senha: await bcrypt.hash("123.456.789-10", Number(process.env.SALT)),
+   matricula: usuario.matricula,
+   email: usuario.email,
+   telefone: usuario.telefone,
+   perfil: usuario.perfil,
+   cpf: usuario.cpf,
+  });
+ }
+}
+(async () => {
+ await criarUsuarios();
+ console.log("Usuarios cadastrados!");
+})();
--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://15.229.118.130:8080', //15.228.159.110
     headers: {
         "Content-Type": 'application/json'
     }
